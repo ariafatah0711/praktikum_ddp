89b3f552c0680726acec005f2999f3bdfecee726
ariafatah0711 <ariafatah0711@gmail.com>
2026-01-06 09:33:53 +0700

    update

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, contact, about
+from .views import home, about, gallery, contact
 
 app_name = 'frontend'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', home, name='index'),
     path('home/', home, name='home'),
     path('about/', about, name='about'),
+    path('gallery/', gallery, name='gallery'),
     path('contact/', contact, name='contact')
 ]
--- app/frontend/views.py
+++ app/frontend/views.py
@@ -10,5 +10,9 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def gallery(request):
+    images = ['1.gif', '2.gif', '3.gif', '4.gif']
+    return render(request, 'gallery.html', {'images': images})
+
 def contact(request):
     return render(request, 'contact.html')
